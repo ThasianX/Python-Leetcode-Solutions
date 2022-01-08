ff581e44c5bf102f7c1c6a0c4236445ef4cb7aeb
Kevin Li <kevinli29033@gmail.com>
2022-01-08 13:35:58 -0800

    FruitIntoBaskets.py & LongestSubstringWithoutRepeatingCharacters.py

---
+++ ContainerWithMostWater.py
@@ -0,0 +1,30 @@
+from typing import List
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+      n = len(height)
+      left_i = 0
+      right_i = n - 1
+      max_area = 0
+
+      while left_i < right_i:
+        left_height = height[left_i]
+        right_height = height[right_i]
+        max_area = max(max_area, (right_i - left_i + 1) * min(left_height, right_height))
+        
+        if left_height < right_height:
+          left_i += 1
+        else:
+          right_i -= 1
+
+
+tests = [
+  (
+    ([1,8,6,2,5,4,8,3,7],),
+    49,
+  ),
+  (
+    ([1,1],),
+    1,
+  ),
+]
